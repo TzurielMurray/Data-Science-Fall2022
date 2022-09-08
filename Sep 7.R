6b05ee79d019e81eafa323142675d9b4070c402c
TzurielMurray <78871159+TzurielMurray@users.noreply.github.com>
2022-09-08 12:55:39 -0400

    Add files via upload

---
+++ Sep 7.R
@@ -0,0 +1,18 @@
+# Author: Tzuriel Murray, Date: 9/7/22, Purpose: Calculate correlation analysis
+
+# Reading the data from csv file
+setwd("/Users/tzuri/OneDrive/Documents")
+
+data <- read.csv(file = 'COVID-Data.csv', header = TRUE)
+
+head(data)
+
+# Correlation analysis between confirmed cases and number of deaths
+# Check if there is any relationship between number of deaths and confirmed cases
+
+res <- cor.test(data$Confirmed, data$Deaths, method = "pearson");
+res
+
+# 1, 2, 3, 4, 5: "Spearman"
+
+ggscatter(data, x = "Confirmed", y = "Deaths", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson")
